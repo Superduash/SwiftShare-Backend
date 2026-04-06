216f4f9fbc5f835cb84fa266640ebfa576d5a809
Superduash <aashwinsuperdu@gmail.com>
2026-04-06 22:19:36 +0530

    setup tik

--- config/db.js
+++ config/db.js
@@ -1,2 +1,17 @@
-﻿// MongoDB connection
+﻿const mongoose = require("mongoose");
+
+async function connectDB() {
+	const uri = process.env.MONGODB_URI;
+
+	if (!uri) {
+		throw new Error("MONGODB_URI is not set in environment variables");
+	}
+
+	await mongoose.connect(uri);
+	return mongoose.connection;
+}
+
+module.exports = {
+	connectDB,
+};
 
--- config/redis.js
+++ config/redis.js
@@ -1,2 +1,24 @@
-﻿// Upstash Redis client
+﻿const { Redis } = require("@upstash/redis");
+
+if (!process.env.UPSTASH_REDIS_REST_URL) {
+	throw new Error("UPSTASH_REDIS_REST_URL is not set in environment variables");
+}
+
+if (!process.env.UPSTASH_REDIS_REST_TOKEN) {
+	throw new Error("UPSTASH_REDIS_REST_TOKEN is not set in environment variables");
+}
+
+const redis = new Redis({
+	url: process.env.UPSTASH_REDIS_REST_URL,
+	token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+async function checkRedisConnection() {
+	await redis.ping();
+}
+
+module.exports = {
+	redis,
+	checkRedisConnection,
+};
 
